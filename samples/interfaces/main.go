c3be64dbb2c31edcdd08ce1c4cdc274ab980fb3a
Kirill Trofimov <trofkm@gmail.com>
2025-09-11 18:23:18 +0300

    feat: now can handle interface resolution (#1)

---
+++ samples/interfaces/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "github.com/trofkm/compoapp"
+
+type IStorage interface {
+	Get(id string) string
+}
+
+type Storage struct{}
+
+var _ IStorage = (*Storage)(nil)
+
+func NewStorage() *Storage {
+	return &Storage{}
+}
+
+func (s *Storage) Get(id string) string {
+	return "this is storage"
+}
+
+type Server struct {
+	storage IStorage
+}
+
+func NewServer(storage IStorage) *Server {
+	return &Server{
+		storage,
+	}
+}
+
+type App struct {
+	server *Server
+}
+
+func NewApp(server *Server) *App {
+	return &App{server}
+}
+
+func main() {
+	container := compoapp.NewContainer()
+	container.Debug()
+
+	container.MustProvide(NewStorage)
+	container.MustProvide(NewServer)
+	container.MustProvide(NewApp)
+
+	var app *App
+	container.MustResolve(&app)
+
+}
